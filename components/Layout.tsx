68539a6bf0bcf6ea5752a581b88c1fe1e79def57
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-27 17:40:20 +0100

    Layout component, login & register pages

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import { FC } from 'react'
+import Header from './Header'
+
+interface Props {
+    pageDescription: string,
+    title: string,
+    children: React.ReactNode
+}
+
+const Layout: FC<Props> = ({pageDescription, title, children}) => {
+  return (
+    <>
+    <Head>
+        <meta name="description" content={pageDescription} />
+        <meta name="og:title" content={title} />
+    </Head>
+
+    <Header />
+
+    <main>
+        {children}
+    </main>
+    </>
+  )
+}
+
+export default Layout
