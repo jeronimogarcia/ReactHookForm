e72e9d6be5e0937ac5529f183958890415a4d89c
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-27 18:02:00 +0100

    Header. React Icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,57 @@
 
+import { FaReact } from "react-icons/fa";
+// import { BsCircleFill } from "react-icons/bs";
+// import { getAuth, signOut } from "firebase/auth";
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="flex items-center justify-between h-20 w-full bg-gray-900 shadow-lg px-20 fixed top-0 text-2xl text-green-500">
+      <Link href="/" className="w-60">
+        <div className="flex flex-row gap-2">
+          <span className="flex items-center text-5xl">
+            <FaReact></FaReact>
+          </span>
+          <span className="flex items-center text-2xl">React-hook-form</span>
+        </div>
+      </Link>
+      <ol className="flex flex-row gap-6">
+        <li>
+          <Link href="/auth/login" className="hover:text-red-500">
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link href="/auth/register" className="hover:text-red-500">
+            Register
+          </Link>
+        </li>
+        {/* {usuario && (
+          <li>
+            <button onClick={handleLogOut}>Logout</button>
+          </li>
+        )} */}
+      </ol>
 
-export default Header
+      {/* <div className="flex items-center justify-center w-52 gap-4">
+        {usuario ? (
+          <div className="flex flex-row gap-4">
+            <span>User logged</span>
+            <span className="flex items-center blur-[1px]">
+              <BsCircleFill />
+            </span>
+          </div>
+        ) : (
+          <div className="text-red-600 flex flex-row gap-4">
+            <span>Desconectado</span>
+            <span className="flex items-center blur-[1px]">
+              <BsCircleFill />
+            </span>
+          </div>
+        )}
+      </div> */}
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Layout from "@/components/Layout"
+
 
 export default function Home() {
   return (
-    <>
+    <Layout title='Home Page' pageDescription="Test de reack-hook-form">
       <h1>Hello World</h1>
-    </>
+    </Layout>
   )
 }
