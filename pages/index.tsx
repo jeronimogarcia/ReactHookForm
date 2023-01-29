9d0b96272c6f06210118348d850e62084bb87301
jeronimogarcia <jero_garcia23@hotmail.com>
2023-01-29 15:37:31 +0100

    Testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from "@/components/Layout"
 export default function Home() {
   return (
     <Layout title='Home Page' pageDescription="Test de reack-hook-form">
-      <h1>Hello World</h1>
+      <h1>Probando git</h1>
     </Layout>
   )
 }
