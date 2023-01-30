6998d6b1157c80717cace7f6c0290d6b6a1bed7e
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-30 13:54:01 +0100

    Trying to fix problem with yup password-repeatpassword

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,33 +1,67 @@
 import Layout from "@/components/Layout";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
-import { yupResolver } from '@hookform/resolvers/yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 
-const schema = yup.object({
-  email: yup.string().required("This field must be filled!").email("Email format is invalid"),
-  firstName: yup.string().required("This field must be filled!").min(3, "Min 3 characters!"),
-  lastName: yup.string().required("This field must be filled!").min(3, "Min 3 characters!"),
-  password: yup.string().required("This field must be filled!").min(6, "Min 6 characters!"),
-  repeatPassword: yup.string()
-  .oneOf([yup.ref('password'), null], 'Passwords must match!'),
-  area: yup.string().required(),
-}).required();
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .required("This field must be filled!")
+      .email("Email format is invalid"),
+    firstName: yup
+      .string()
+      .required("This field must be filled!")
+      .min(3, "Min 3 characters!"),
+    lastName: yup
+      .string()
+      .required("This field must be filled!")
+      .min(3, "Min 3 characters!"),
+    password: yup
+      .string()
+      .required("This field must be filled!")
+      .min(6, "Min 6 characters!"),
+    repeatPassword: yup
+      .string()
+      .required("This field must be filled!")
+      .oneOf([yup.ref("password"), null], "Passwords must match!"),
+    area: yup.string().required(),
+  })
+  .required();
 type FormData = yup.InferType<typeof schema>;
 
-const inputStyle = "border border-gray-500 px-1 rounded outline-none bg-gray-200"
-const labelStyle = "text-lg mt-1 mb-2 text-green-500"
-const errorStyle = "mt-2 text-red-500"
+const inputStyle =
+  "border border-gray-500 px-1 rounded outline-none bg-gray-200";
+const labelStyle = "text-lg mt-1 mb-2 text-green-500";
+const errorStyle = "mt-2 text-red-500";
 
 const RegisterPage = () => {
-  const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    getValues,
+  } = useForm<FormData>({
     mode: "all",
-    resolver: yupResolver(schema)
+    resolver: yupResolver(schema),
   });
   const onSubmit = (data: FormData) => {
-    console.log(data)
-    reset() 
+    console.log(data);
+    reset();
   };
 
+  const validatePasswords = (value1: string, value2: string) => {
+      if(value1 !== value2){
+        console.log("diferentes")
+        console.log(errors.repeatPassword?.message)
+        console.log(errors.repeatPassword)
+      } else {
+        console.log("iguales")
+      
+      }
+    
+  };
 
   return (
     <Layout
@@ -40,50 +74,34 @@ const RegisterPage = () => {
       >
         <h1 className="text-2xl text-green-500 mb-4">Register Form</h1>
         <label className={labelStyle}>Email</label>
-        <input
-          {...register("email", {})}
-          className={inputStyle}
-        />
+        <input {...register("email", {})} className={inputStyle} />
         {!errors.email && <p className="h-6"></p>}
-        <p className={errorStyle} >{errors.email?.message}</p>
+        <p className={errorStyle}>{errors.email?.message}</p>
         <label className={labelStyle}>First Name</label>
-        <input
-          {...register("firstName", {})}
-          className={inputStyle}
-        />
+        <input {...register("firstName", {})} className={inputStyle} />
         {!errors.firstName && <p className="h-6"></p>}
-        <p className={errorStyle} >{errors.firstName?.message}</p>
-        {/* {errors.firstName?.message && <p className={errorStyle}>First name required!</p>} */}
-         {/* {errors.firstName?.type === 'minLength' && <p className={errorStyle}>Too short, 3 characters minimum.</p>} */}
+        <p className={errorStyle}>{errors.firstName?.message}</p>
         <label className={labelStyle}>Last Name</label>
-        <input
-          {...register("lastName", {})}
-          className={inputStyle}
-        />
-         {!errors.lastName && <p className="h-6"></p>}
-        <p className={errorStyle} >{errors.lastName?.message}</p>
+        <input {...register("lastName", {})} className={inputStyle} />
+        {!errors.lastName && <p className="h-6"></p>}
+        <p className={errorStyle}>{errors.lastName?.message}</p>
 
         <label className={labelStyle}>Password</label>
         <input
           {...register("password", {})}
           className={inputStyle}
+          onChange={(e) => validatePasswords(e.target.value, getValues("repeatPassword"))}
         />
-         {!errors.password && <p className="h-6"></p>}
-        <p className={errorStyle} >{errors.password?.message}</p>
+        {!errors.password && <p className="h-6"></p>}
+        <p className={errorStyle}>{errors.password?.message}</p>
 
         <label className={labelStyle}>Repeat Password</label>
-        <input
-          {...register("repeatPassword", {})}
-          className={inputStyle}
-        />
-         {!errors.repeatPassword && <p className="h-6"></p>}
-        <p className={errorStyle} >{errors.repeatPassword?.message}</p>
+        <input {...register("repeatPassword", {})} className={inputStyle} />
+        {!errors.repeatPassword && <p className="h-6"></p>}
+        <p className={errorStyle}>{errors.repeatPassword?.message}</p>
 
-        <label  className={labelStyle}>Programming Area</label>
-        <select
-          {...register("area")}
-          className={inputStyle}
-        >
+        <label className={labelStyle}>Programming Area</label>
+        <select {...register("area")} className={inputStyle}>
           <option value="frontend">Frontend</option>
           <option value="backend">Backend</option>
           <option value="fullstack">Fullstack</option>
@@ -94,7 +112,7 @@ const RegisterPage = () => {
           onClick={() => onSubmit}
         >
           Registrar Usuario
-       </button>   
+        </button>
       </form>
     </Layout>
   );
