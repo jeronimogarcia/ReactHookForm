11a76cbff1bb281fbed78a2e528075d2ccbca1a9
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-31 15:10:38 +0100

    Disabled repeat password

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,33 +1,61 @@
 import Layout from "@/components/Layout";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { useState, useEffect } from 'react';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { useState, useEffect } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
-const schema = yup.object({
-  password: yup.string().required("This field must be filled!").min(6, "Min 6 characters!"),
-  repeatPassword: yup.string()
-  .oneOf([yup.ref('password'), null], 'Passwords must match!'),
-}).required();
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .required("This field must be filled!")
+      .email("Email format is invalid"),
+    firstName: yup
+      .string()
+      .required("This field must be filled!")
+      .min(3, "Min 3 characters!"),
+    lastName: yup
+      .string()
+      .required("This field must be filled!")
+      .min(3, "Min 3 characters!"),
+    password: yup
+      .string()
+      .required("This field must be filled!")
+      .min(6, "Min 6 characters!"),
+    repeatPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords must match!"),
+    area: yup.string().required(),
+  })
+  .required();
 
 type FormData = yup.InferType<typeof schema>;
 
-
-const inputStyle = "border border-gray-500 px-1 rounded outline-none bg-gray-200";
+const inputStyle =
+  "border border-gray-500 px-1 rounded outline-none bg-gray-200 disabled:opacity-70";
 const labelStyle = "text-lg mt-1 mb-2 text-green-500";
 const errorStyle = "mt-2 text-red-500";
 
 const RegisterPage = () => {
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
     watch,
-    trigger
+    trigger,
   } = useForm<FormData>({
     resolver: yupResolver(schema),
     mode: "all",
+    defaultValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+      repeatPassword: '',
+      area: 'FullStack'
+    }
   });
   const onSubmit: SubmitHandler<FormData> = (data) => {
     console.log(data);
@@ -39,7 +67,7 @@ const RegisterPage = () => {
   useEffect(() => {
     trigger("repeatPassword");
   }, [password, trigger]);
-  
+
   return (
     <Layout
       title="Register Page"
@@ -50,23 +78,47 @@ const RegisterPage = () => {
         className="flex align-center flex-col w-[800px] p-8 pb-8 rounded bg-gray-900 relative"
       >
         <h1 className="text-2xl text-green-500 mb-4">Register Form</h1>
-        
-        <label className={labelStyle}>Password</label>
+
+        <label className={labelStyle}>Email</label>
+        <input {...register("email", {})} className={inputStyle} />
+        {!errors.email && <p className="h-6"></p>}
+        <p className={errorStyle}>{errors.email?.message}</p>
+
+        <label className={labelStyle}>First Name</label>
         <input
-          {...register("password", {})}
+          {...register("firstName", { required: true, minLength: 3 })}
           className={inputStyle}
         />
+        {!errors.firstName && <p className="h-6"></p>}
+        <p className={errorStyle}>{errors.firstName?.message}</p>
+
+        <label className={labelStyle}>Last Name</label>
+        <input
+          {...register("lastName", { required: true })}
+          className={inputStyle}
+        />
+        {!errors.lastName && <p className="h-6"></p>}
+        <p className={errorStyle}>{errors.lastName?.message}</p>
+
+        <label className={labelStyle}>Password</label>
+        <input {...register("password", {})} className={inputStyle} />
         {!errors.password && <p className="h-6"></p>}
-        <p className={errorStyle} >{errors.password?.message}</p>
+        <p className={errorStyle}>{errors.password?.message}</p>
 
         <label className={labelStyle}>Repeat Password</label>
-        <input
-          {...register("repeatPassword")}
-          className={inputStyle}
-          
+        <input {...register("repeatPassword")}
+        disabled={password.length < 6}
+        className={inputStyle}
         />
         {!errors.repeatPassword && <p className="h-6"></p>}
-        <p className={errorStyle} >{errors.repeatPassword?.message}</p>
+        <p className={errorStyle}>{errors.repeatPassword?.message}</p>
+
+        <label className={labelStyle}>Programming Area</label>
+        <select {...register("area")} className={inputStyle}>
+          <option value="frontend">Frontend</option>
+          <option value="backend">Backend</option>
+          <option value="fullstack">Fullstack</option>
+        </select>
 
         <button
           type="submit"
