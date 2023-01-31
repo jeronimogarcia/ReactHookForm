ec9f35468dd2da43659eb8d796124c3bf7c8df62
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-31 17:10:34 +0100

    Firebase install

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQZ5phOK5XyXjvzy-ugzsTGTLqbqTRjbY",
+  authDomain: "reacthookform-d0c4c.firebaseapp.com",
+  projectId: "reacthookform-d0c4c",
+  storageBucket: "reacthookform-d0c4c.appspot.com",
+  messagingSenderId: "676215890291",
+  appId: "1:676215890291:web:472d01852fde7ec1336506"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
